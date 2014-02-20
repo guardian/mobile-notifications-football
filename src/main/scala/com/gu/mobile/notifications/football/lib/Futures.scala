7ffcef1699f33e095e9867a941de2d7c9eaa3bee
Robert Berry <robert.berry@guardian.co.uk>
2014-02-20 15:31:23 +0000

    Add topic auditing endpoint

--- src/main/scala/com/gu/mobile/notifications/football/lib/Futures.scala
+++ src/main/scala/com/gu/mobile/notifications/football/lib/Futures.scala
@@ -20,4 +20,22 @@ object Futures {
       }
     }
   }
+
+  implicit class RichFutureCompanion(companion: Future.type) {
+    /** Like Future.sequence, but silently ignores failures, collecting all successful results */
+    def sequenceSuccessful[A](futures: Seq[Future[A]])(implicit executionContext: ExecutionContext): Future[List[A]] = {
+      def iter(futures: Seq[Future[A]], acc: List[A]): Future[List[A]] = {
+        futures match {
+          case fa :: fas => fa flatMap { a: A =>
+            iter(fas, a :: acc)
+          } recoverWith {
+            case _ => iter(fas, acc)
+          }
+          case Nil => Future.successful(acc.reverse)
+        }
+      }
+
+      iter(futures, Nil)
+    }
+  }
 }
---
+++ src/test/scala/com/gu/mobile/notifications/football/lib/PaExpiringTopicsSpec.scala
@@ -0,0 +1,115 @@
+package com.gu.mobile.notifications.football.lib
+
+import org.scalatest.{Matchers, WordSpec}
+import pa._
+import scala.concurrent.{ExecutionContext, Future, Await}
+import ExecutionContext.Implicits.global
+import org.joda.time.DateTime
+import pa.MatchDay
+import pa.Response
+import com.gu.mobile.notifications.client.models.Topic
+import scala.concurrent.duration.Duration
+
+class PaExpiringTopicsSpec extends WordSpec with Matchers {
+  trait NoHttp extends Http {
+    override def GET(url: String): Future[Response] = Future.failed(new RuntimeException("This is a fixture HTTP only"))
+  }
+
+  def matchInfoFixtureClient(matchDays: List[MatchDay]) = {
+    PaExpiringTopics(new PaClient with NoHttp {
+      val apiKey = ""
+
+      override def matchInfo(id: String)(implicit context: ExecutionContext) = {
+        matchDays.find(_.id == id) match {
+          case Some(matchDay) => Future.successful(matchDay)
+          case None => Future.failed(new RuntimeException(s"Could not find match day with id $id"))
+        }
+      }
+    })
+  }
+
+  implicit class RichMatchDayTeamCompanion(companion: MatchDayTeam.type) {
+    def empty = MatchDayTeam(
+      "",
+      "",
+      None,
+      None,
+      None,
+      None
+    )
+  }
+
+  implicit class RichMatchDayCompanion(companion: MatchDay.type) {
+    def empty = MatchDay(
+      "",
+      new DateTime(),
+      None,
+      None,
+      "",
+      false,
+      false,
+      false,
+      false,
+      false,
+      "",
+      None,
+      MatchDayTeam.empty,
+      MatchDayTeam.empty,
+      None,
+      None,
+      None
+    )
+
+    def forIdAndStatus(id: String, status: String) = empty.copy(id = id, matchStatus = status)
+  }
+
+  "PaExpiringTopics" should {
+    "return a list of topics for which there were FT statuses" in {
+      val fixtureClient = matchInfoFixtureClient(List(
+        MatchDay.forIdAndStatus("1", "FT"),
+        MatchDay.forIdAndStatus("2", "KO"),
+        MatchDay.forIdAndStatus("3", "Abandoned"),
+        MatchDay.forIdAndStatus("4", "HT"),
+        MatchDay.forIdAndStatus("5", "Cancelled")
+      ))
+
+      val topic1 = Topic(
+        "football-match",
+        "1"
+      )
+      val topic2 = Topic(
+        "football-match",
+        "2"
+      )
+      val topic3 = Topic(
+        "football-match",
+        "3"
+      )
+      val topic4 = Topic(
+        "football-match",
+        "4"
+      )
+      val topic5 = Topic(
+        "football-match",
+        "5"
+      )
+      val topic6 = Topic(
+        "football-match",
+        "6"
+      )
+
+      Await.result(fixtureClient.getExpired(List(
+        topic1,
+        topic2,
+        topic3,
+        topic4,
+        topic5,
+        topic6
+      )), Duration.Inf) should be(List(
+        topic1,
+        topic3,
+        topic5
+      ))
+    }
+  }
+}
