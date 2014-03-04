fa6aeb8eaaddf7144ed69875cff37940e5fe6277
Robert Berry <robert.berry@guardian.co.uk>
2014-03-04 13:59:46 +0000

    Add better logging for when goals are scored

--- src/main/scala/com/gu/mobile/notifications/football/GoalNotificationsPipeline.scala
+++ src/main/scala/com/gu/mobile/notifications/football/GoalNotificationsPipeline.scala
@@ -1,6 +1,7 @@
 package com.gu.mobile.notifications.football
 
 import scala.concurrent.duration._
+import com.gu.mobile.notifications.football.models.{EventFeedMetadata, OwnGoal, Goal}
 
 object GoalNotificationsPipeline extends GoalNotificationsPipeline {
   val MaxHistoryLength: Int = 100
@@ -17,8 +18,20 @@ trait GoalNotificationsPipeline extends Streams with SNSQueue with GoalNotificat
     val notificationSendHistory = getNotificationResponses(notificationStream)
 
     notificationSendHistory.subscribe(logNotificationHistory _)
-   // goalEventStream.subscribe(logGoalEvents _)
-   // matchDayStream.subscribe(logLastMatchDay _)
+
+    goalEventStream.subscribe({ pair =>
+      val (event, EventFeedMetadata(matchID, homeTeam, homeScore, awayTeam, awayScore)) = pair
+
+      event match {
+        case Goal(scorerName, scoringTeam, otherTeam, minute) =>
+          logger.info(s"$matchID >>> $scorerName scored for ${scoringTeam.name} at $minute min: " +
+            s"${homeTeam.name} $homeScore - $awayScore ${awayTeam.name}")
+        case OwnGoal(scorerName, scoringTeam, otherTeam, minute) =>
+          logger.info(s"$matchID >>> $scorerName (${otherTeam.name}}) scored an own goal! " +
+            s"${homeTeam.name} $homeScore - $awayScore ${awayTeam.name}")
+      }
+    })
+
     notificationStream.subscribe(publishNotifications _)
   }
 }
