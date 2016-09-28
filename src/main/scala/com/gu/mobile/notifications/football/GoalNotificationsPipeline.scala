e2f8c878fce18fa0f1c634ea78223ee7c3b8faf0
Alexandre Dufournet <alex.dufournet@theguardian.com>
2016-09-28 17:06:55 +0100

    reduce rate by tenfold

--- src/main/scala/com/gu/mobile/notifications/football/GoalNotificationsPipeline.scala
+++ src/main/scala/com/gu/mobile/notifications/football/GoalNotificationsPipeline.scala
@@ -7,7 +7,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
 object GoalNotificationsPipeline extends GoalNotificationsPipeline {
   val MaxHistoryLength: Int = 100
   val retrySendNotifications: Int = 5
-  val UpdateInterval = 3.seconds
+  val UpdateInterval = 30.seconds
 }
 
 trait GoalNotificationsPipeline extends Streams with SNSQueue with GoalNotificationLogger {
