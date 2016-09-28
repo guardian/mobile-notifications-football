4cd4a44b89795fd5b6df141884d841bc057ca7c0
Alexandre Dufournet <alex.dufournet@theguardian.com>
2016-09-28 17:07:12 +0100

    Load the host from the conf

--- src/main/scala/com/gu/mobile/notifications/football/lib/PaClient.scala
+++ src/main/scala/com/gu/mobile/notifications/football/lib/PaClient.scala
@@ -17,6 +17,7 @@ trait DispatchHttp extends pa.Http with Logging {
 
 object PaFootballClient extends PaClient with DispatchHttp {
   def apiKey = GoalNotificationsConfig.paApiKey
+  override lazy val base: String = GoalNotificationsConfig.paHost
 
   override protected def get(suffix: String)(implicit context: ExecutionContext): Future[String] = super.get(suffix)(context)
 }
