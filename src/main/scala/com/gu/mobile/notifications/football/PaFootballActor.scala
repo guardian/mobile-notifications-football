fb3fb35d15117713a321177e824f348e96de752a
Alexandre Dufournet <alex.dufournet@theguardian.com>
2017-04-28 14:19:25 +0100

    Ignore games that have finished long ago

--- src/main/scala/com/gu/mobile/notifications/football/PaFootballActor.scala
+++ src/main/scala/com/gu/mobile/notifications/football/PaFootballActor.scala
@@ -72,7 +72,8 @@ class PaFootballActor(
 
   private def matchIdsInProgress: Future[List[MatchId]] = {
     def inProgress(m: MatchDay): Boolean =
-      m.date.minusMinutes(5).isBefore(DateTime.now) && !endedMatches.exists(_.matchId == m.id)
+      m.date.minusMinutes(5).isBeforeNow &&
+        (!endedMatches.exists(_.matchId == m.id) || m.date.plusHours(4).isBeforeNow)
     val matches = cachedMatches() {
       logger.info("Retrieving today's matches from PA")
       paClient.aroundToday recover {
