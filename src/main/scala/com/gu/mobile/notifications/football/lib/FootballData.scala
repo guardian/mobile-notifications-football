faf38a97145ec5b6c221e04b1eaac0e55d55fd1a
Nathaniel Bennett <nathaniel.bennett@guardian.co.uk>
2018-03-02 11:09:15 +0000

    filter unwanted games

--- src/main/scala/com/gu/mobile/notifications/football/lib/FootballData.scala
+++ src/main/scala/com/gu/mobile/notifications/football/lib/FootballData.scala
@@ -17,6 +17,43 @@ class FootballData(
   syntheticEvents: SyntheticMatchEventGenerator
 ) extends Logging {
 
+  implicit class RichMatchDay(matchDay: MatchDay) {
+
+    private lazy val internationalTeams = Set(
+      "England",
+      "Scotland",
+      "Wales",
+      "Rep of Ireland",
+      "Spain",
+      "Germany",
+      "Italy",
+      "France",
+      "Belgium",
+      "Portugal",
+      "Turkey",
+      "Poland",
+      "Norway",
+      "Sweden",
+      "Denmark",
+      "Russia",
+      "Argentina",
+      "Brazil"
+    )
+
+    def isUncoveredInternationalFriendly: Boolean = Set(matchDay.homeTeam.name, matchDay.awayTeam.name).intersect(internationalTeams).isEmpty
+
+    def isEarlyQualifyingRound: Boolean = competitionRoundToInt.map( r => r < 3 ).getOrElse(true)
+
+    private def competitionRoundToInt: Option[Int] = {
+      try {
+        Some(matchDay.round.roundNumber.toInt)
+      } catch {
+        case e: Exception => None
+      }
+    }
+  }
+
+
   def pollFootballData: Future[List[RawMatchData]] = {
     logger.info("Starting poll for new match events")
 
@@ -34,13 +71,8 @@ class FootballData(
   }
 
   private def matchIdsInProgress: Future[List[MatchDay]] = {
-    def inProgress(m: MatchDay): Boolean = {
-      m.competition match {
-        case Some(c) => logger.info(s"Match: ${m.id} Competition: ${c.id} Round: ${m.round.roundNumber} Home: (Name: ${m.homeTeam.name} Id: ${m.homeTeam.id})  Away: (Name: ${m.awayTeam.name}, id: ${m.awayTeam.id}")
-        case _ => logger.info(s"Match: ${m.id} No Competition")
-      }
+    def inProgress(m: MatchDay): Boolean =
       m.date.minusMinutes(5).isBeforeNow && m.date.plusHours(4).isAfterNow
-    }
 
     // unfortunately PA provide 00:00 as start date when they don't have the start date
     // so we can't do anything with these matches
@@ -49,10 +81,27 @@ class FootballData(
       localDate.getHourOfDay == 0 && localDate.getMinuteOfHour == 0
     }
 
+    //Theres some stuff that PA don't provide data for and we want to supress alerts for these matches
+    def paProvideAlerts(matchDay: MatchDay) : Boolean = {
+      matchDay.competition.map {
+        c => c.id match {
+          //International friendly: Must involve one of a specific set of teams
+          case "721" if matchDay.isUncoveredInternationalFriendly => false
+
+          //FA cup qualifying rounds not covererd before round 3
+          case "303" if matchDay.isEarlyQualifyingRound => false
+
+          case _ => true
+        }
+      }.getOrElse(false) //Shouldn't ever happen
+    }
+
     logger.info("Retrieving today's matches from PA")
     val matches = paClient.aroundToday
     matches.map(
-      _.filter(inProgress).filterNot(isMidnight)
+      _.filter(inProgress)
+       .filter(paProvideAlerts)
+       .filterNot(isMidnight)
     )
   }
 
